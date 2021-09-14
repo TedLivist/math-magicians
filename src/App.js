6b7fa510e29b3fd511e88a840d4beba60f808c49
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2021-09-14 15:57:36 +0100

    Delete boilerplate files and refactor App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import React from 'react';
 import Calculator from './components/calculator';
 import './App.css';
 
-function App() {
-  return (
-    <Calculator />
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <Calculator />;
+  }
 }
 
 export default App;
