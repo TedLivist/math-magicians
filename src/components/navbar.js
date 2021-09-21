89a819758717992904e8142d3a20061cb8663ba4
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2021-09-21 13:54:31 +0100

    Remove linter disablements

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,3 @@
-/* eslint-disable arrow-body-style */
-
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import '../stylesheets/navbar.css';
@@ -29,13 +27,11 @@ const Navbar = () => {
         <h1>Math Magicians</h1>
       </div>
       <ul className="menu-items">
-        {links.map((link) => {
-          return (
-            <li key={link.id} className={link.id === 3 ? '' : 'link-border'}>
-              <NavLink to={link.path} className="link-margin">{link.text}</NavLink>
-            </li>
-          );
-        })}
+        {links.map((link) => (
+          <li key={link.id} className={link.id === 3 ? '' : 'link-border'}>
+            <NavLink to={link.path} className="link-margin">{link.text}</NavLink>
+          </li>
+        ))}
       </ul>
     </nav>
   );
--- src/pages/quote.js
+++ src/pages/quote.js
@@ -1,17 +1,14 @@
-/* eslint-disable arrow-body-style, react/jsx-one-expression-per-line */
-
 import React from 'react';
 import '../stylesheets/quote.css';
 
-const Quote = () => {
-  return (
-    <div className="quote-container">
-      <h2>Mathematics possesses not only truth,
-        but supreme beauty -- a beauty cold and austere,
-        like that of sculpture. -- Bertrand Russell
-      </h2>
-    </div>
-  );
-};
+const Quote = () => (
+  <div className="quote-container">
+    <h2>
+      Mathematics possesses not only truth,
+      but supreme beauty -- a beauty cold and austere,
+      like that of sculpture. -- Bertrand Russell
+    </h2>
+  </div>
+);
 
 export default Quote;
