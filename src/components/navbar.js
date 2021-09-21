94cf89868ef3b7dbdd59fb8513cf9e7aa6dad053
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2021-09-21 10:31:37 +0100

    Add links to navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,10 +1,39 @@
 /* eslint-disable arrow-body-style */
 
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
+  const links = [
+    {
+      id: 1,
+      path: '/',
+      text: 'Home',
+    },
+    {
+      id: 2,
+      path: '/calculator',
+      text: 'Calculator',
+    },
+    {
+      id: 3,
+      path: '/quote',
+      text: 'Quote',
+    },
+  ];
+
   return (
-    <div>Navbar</div>
+    <nav>
+      <ul>
+        {links.map((link) => {
+          return (
+            <li key={link.id}>
+              <NavLink to={link.path}>{link.text}</NavLink>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
   );
 };
 
