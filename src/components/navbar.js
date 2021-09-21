53b6d6608d14cb43b14857b8dc982958dfcfbe42
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2021-09-21 10:24:01 +0100

    Add Routers

---
+++ src/components/navbar.js
@@ -0,0 +1,11 @@
+/* eslint-disable arrow-body-style */
+
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/quote.js
@@ -0,0 +1,13 @@
+/* eslint-disable arrow-body-style */
+
+import React from 'react';
+
+const Quote = () => {
+  return (
+    <div>
+      <h3>Quote</h3>
+    </div>
+  );
+};
+
+export default Quote;
